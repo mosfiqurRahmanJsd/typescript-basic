3652de37b9d5b00daf7db03b58dc641a6910aca6
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-07-20 02:45:47 +0600

    arrow function

--- first.ts
+++ first.ts
@@ -14,3 +14,18 @@ function fullName (firstName: string, lastName: string): string {
 }
 
 const user = fullName('Ben', 'Adam'); 
+
+
+function doubleItAndConsole(num:number): void {
+    const result = num * 2; 
+    console.log(result);
+}
+const output = doubleItAndConsole(10);
+console.log(output);
+
+
+let multiply2: (x: number, y: number) => number; 
+multiply2 = (x, y) => x * y; 
+
+const multiply = (x: number, y: number) : number => x * y; 
+
