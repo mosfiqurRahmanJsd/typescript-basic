1f03e555ac6796d538c0ae5a20ef4d422e2de22d
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-07-20 02:36:23 +0600

    function added

--- first.ts
+++ first.ts
@@ -1,3 +1,16 @@
 let age:number = 50; 
 let club:string = 'Real Madrid'; 
 const isFamous: boolean = false; 
+
+
+function add (num1: any, num2: number | string) {
+    return num1 + num2;
+}
+add(3, 76); 
+
+
+function fullName (firstName: string, lastName: string): string {
+    return firstName + " " + lastName; 
+}
+
+const user = fullName('Ben', 'Adam'); 
