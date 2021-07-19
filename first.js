18b6ae1558e7db6e118f5695b9c1d99090fd7274
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-07-20 02:49:03 +0600

    ts to js update

--- first.js
+++ first.js
@@ -1,3 +1,20 @@
-var myMoney = 50;
-myMoney = 150;
-myMoney = 0;
+var age = 50;
+var club = 'Real Madrid';
+var isFamous = false;
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(3, 76);
+function fullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+var user = fullName('Ben', 'Adam');
+function doubleItAndConsole(num) {
+    var result = num * 2;
+    console.log(result);
+}
+var output = doubleItAndConsole(10);
+console.log(output);
+var multiply2;
+multiply2 = function (x, y) { return x * y; };
+var multiply = function (x, y) { return x * y; };
